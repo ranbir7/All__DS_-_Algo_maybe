b044bb8e000b9f19473445192a911feef17abf05
Ranbir <71406920+ranbir7@users.noreply.github.com>
2022-09-24 03:02:59 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,48 +1,43 @@
-class Graph:
+class graph:
     def __init__(self):
-        self.nodes = 0
-        self.GraphC = {}
+        self.graph = dict()
 
-    def addVertex(self, key_value):
-        if key_value in self.GraphC.keys():
-            print('Duplicate Vertexes Are Not Allowed')
+    def addVertex(self, node):
+        self.graph[node] = list()
 
+    def addEdge(self, node, value):
+        if value in self.graph[node]:
             return
-        else:
-            self.GraphC[key_value] = []
-            self.nodes += 1
+        self.graph[node].append(value)
+        if value in self.graph.keys():
+            self.graph[value].append(node)
 
-    def addEdge(self, key_value, value_member):
-        if key_value in self.GraphC.keys():
-            self.GraphC[key_value] += value_member
-            self.GraphC[value_member] += key_value
-            return
-        else:
-            print('Duplicate Edges Are Not Allowed')
-            return
-
-    def showGraph(self):
-        for i in self.GraphC:
+    def printGraph(self):
+        for i in self.graph.keys():
+            print(f'{i}-->{self.graph[i]}')
+    
+    #Previously created Function printGrph
+    """
+       def showGraph(self):
+          for i in self.GraphC:
             print('{} --> {}'.format(i, list(set(self.GraphC[i]))))
-
-
-c = Graph()
+    """
+c= graph()          
 c.addVertex('0')
-c.addVertex('1')
-c.addVertex('2')
-c.addVertex('3')
-c.addVertex('4')
-c.addVertex('5')
-c.addVertex('6')
-c.addEdge('3', '1')
-c.addEdge('3', '4')
-c.addEdge('0', '1')
-c.addEdge('0', '2')
+c.addVertex('1') 
+c.addVertex('2') 
+c.addVertex('3') 
+c.addVertex('4') 
+c.addVertex('5') 
+c.addVertex('6') 
+c.addEdge('3', '1') 
+c.addEdge('3', '4') 
+c.addEdge('0', '1') 
+c.addEdge('0', '2') 
 c.addEdge('2', '4')
-c.addEdge('2', '1')
-c.addEdge('4', '5')
-c.addEdge('5', '6')
-c.addEdge('1','2')
+c.addEdge('2', '1') 
+c.addEdge('4', '5') 
+c.addEdge('5', '6') 
+c.addEdge('1','2') 
 c.addEdge('1','3')
-c.showGraph()
-print('The Number Of Vertexes is:',c.nodes)
+c.printGraph()
